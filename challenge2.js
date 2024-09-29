af5642b1e5a7e111ad23c42bd1303b113c6c43ff
Jane-Njoroge <njorogejwangui@gmail.com>
2024-09-29 20:44:51 +0300

    Add files via upload

---
+++ challenge2.js
@@ -0,0 +1,13 @@
+function speedCheck(speed) {const speedLimit=70
+    if (speeed <= speedLimit) {console.log("ok")
+    }else {
+let demeritPoints;
+demeritPoints=
+Math.floor((speed-speedLimit)/5)
+if(demeritPoints>12) {
+    console.log("License suspended")
+}else {
+    console.log(points; ${demeritPoints})
+}
+}
+}
